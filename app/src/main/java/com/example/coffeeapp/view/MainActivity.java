557ebb6953140b5719c04fc9524379a32b16a6d7
dacduc <ducnguyen.12965@gmail.com>
2023-03-29 17:36:32 +0700

    update order_by_employee and add revenue_statistics 29.03.2023

--- app/src/main/java/com/example/coffeeapp/view/MainActivity.java
+++ app/src/main/java/com/example/coffeeapp/view/MainActivity.java
@@ -1,16 +1,15 @@
-package com.example.coffeeapp;
-
-//import android.support.v7.app.AppCompatActivity;
-
-import android.os.Bundle;
+package com.example.coffeeapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.coffeeapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.food_item);
+        setContentView(R.layout.revenue_statistics);
     }
 }
