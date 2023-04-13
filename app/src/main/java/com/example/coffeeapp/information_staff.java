48b01f8202f6c51aecfdd005876760990b3ce973
HoangVu <nguyenhoangvutl@gmail.com>
2023-04-13 09:51:46 +0700

    vu gui 13.4.2023

--- app/src/main/java/com/example/coffeeapp/information_staff.java
+++ app/src/main/java/com/example/coffeeapp/information_staff.java
@@ -8,19 +8,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.coffeeapp.view.EmployeeTimekeepingV;
+import com.example.coffeeapp.view.OrderByEmployee;
+import com.example.coffeeapp.view.RevenueView;
+
 public class information_staff extends Activity {
-    private Button btnallstaff;
+    private Button btnnhanvien,btndiemdanh,btnsanpham,btnthongtin,btndoanhthu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_information_staff);
-        btnallstaff = findViewById(R.id.btn_all_staff);
-        btnallstaff.setOnClickListener(new View.OnClickListener() {
+        btnnhanvien = findViewById(R.id.btn_nhanvien);
+        btnnhanvien.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(information_staff.this,AllStaff.class);
+                startActivity(intent);
+            }
+        });
+        btndiemdanh = findViewById(R.id.btn_diemdanh);
+        btndiemdanh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(information_staff.this, EmployeeTimekeepingV.class);
+                startActivity(intent);
+            }
+        });
+        btnsanpham = findViewById(R.id.btn_sanpham);
+        btnsanpham.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(information_staff.this, OrderByEmployee.class);
+                startActivity(intent);
+            }
+        });
+        btndoanhthu = findViewById(R.id.btn_doanhthu);
+        btndoanhthu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(information_staff.this, RevenueView.class);
                 startActivity(intent);
             }
         });
